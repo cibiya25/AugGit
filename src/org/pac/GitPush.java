506088c3f548c927ce1095a65afff8768ec82cd9
cibiya25 <sibimurugas@gmail.com>
2022-08-03 15:10:30 +0530

    task

---
+++ src/org/pac/GitPush.java
@@ -0,0 +1,16 @@
+package org.pac;
+
+
+
+public class GitPush {
+	private void desktopSize() {
+		System.out.println("desktop size is less than 21inch");
+
+	}
+	public static void main(String[] args) {
+		GitPush c=new GitPush();
+		c.desktopSize();
+		
+	}
+
+}
