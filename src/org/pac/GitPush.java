0eaae958cb65ae185d05018df37e7909d8ea93ce
cibiya25 <sibimurugas@gmail.com>
2022-08-03 16:21:20 +0530

    new

--- src/org/pac/GitPush.java
+++ src/org/pac/GitPush.java
@@ -3,14 +3,13 @@ package org.pac;
 
 
 public class GitPush {
-	private void desktopSize() {
-		System.out.println("desktop size is less than 21inch");
+	private void deposit() {
+		System.out.println("deposit amount is 250000");
 
 	}
 	public static void main(String[] args) {
 		GitPush c=new GitPush();
-		c.desktopSize();
-		
+		c.deposit();
 	}
 
 }
